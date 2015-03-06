a3452c4967836daab2e5a31049b312a40f115554
jblancolopez <j.blancolopez@campus.unimib.it>
2015-03-06 18:46:09 +0100

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,22 @@
+# S E T   W O R K I N G    D I R
+setwd(".../4_Exploratory Data Analysis")
+
+# R E A D   D A T A   S E T
+power_consumption <- read.csv("./Project1/household_power_consumption.txt", header=T, sep=";", stringsAsFactors=F, dec=".")
+
+# S U B S E T    D A T A   S E T
+consumption <- power_consumption[power_consumption$Date %in% c("1/2/2007","2/2/2007") ,]
+
+attach(consumption)
+x <- paste(Date, Time)
+consumption$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(consumption) <- 1:nrow(consumption)
+
+attach(consumption)
+###################################
+#  P L O T    2 ***** DATETIME THUR-SAT
+##################################
+
+png("plot2.png", width = 480, height = 480, units = "px", bg = "transparent")
+plot(DateTime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
